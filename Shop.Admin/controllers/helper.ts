feb83aaf1184b07bc7fc33779145d9a9bb390855
Evgenii <legkovallod@mail.ru>
2025-08-30 00:23:34 +0300

    sf-scan-project
    sf-scan-project

--- Shop.Admin/controllers/helper.ts
+++
@@ -1,7 +0,0 @@
-import { Response } from 'express';
-
-export const throwServerError = (res: Response, e: Error) => {
-  console.debug(e.message);
-  res.status(500);
-  res.send('Something went wrong');
-};
--- Shop.Admin/models/const.ts
+++
@@ -1,1 +0,0 @@
-export const API_HOST = `http://${process.env.LOCAL_HOST}:${process.env.LOCAL_PORT}/${process.env.API_PATH}`;
